56fc78f01154b86d1dee524718a017007ad0e490
Atul Kumar Singh <65683432+aksxy007@users.noreply.github.com>
2025-05-01 22:38:13 +0530

    resume change

--- components/intro.tsx
+++ components/intro.tsx
@@ -61,7 +61,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Atul Kumar Singh.</span> I'm a{" "}
         <span className="font-bold">full-stack Data Science developer</span>{" "}
-        with <span className="font-bold">1.5 years</span> of experience. I enjoy
+        with <span className="font-bold">1.9 years</span> of experience. I enjoy
         building{" "}
         <span className="italic">
           sites & apps and solve real-world problems through machine learning
